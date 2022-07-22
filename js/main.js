79f06150bd552d9dfe4b74fdf8403d7495dbb6de
Maximiliano <brotzumaxx@gmail.com>
2022-07-22 15:49:26 -0300

    Agregada la libreria Luxon junto con la fecha de la conversion bajo el resultado

--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@ let lblResultado = document.getElementById("lblResultadoConversion");
 let btnConvertir = document.getElementById("btnConvertir");
 
 const divisas = [];
+const DateTime = luxon.DateTime;
 let conversionesRecientes;
+let horariosRecientes;
 
 class Divisa {
     constructor(nombre, abreviatura, simbolo, valorRelativo) {
@@ -20,12 +22,13 @@ function GenerarDivisas() {
     divisas.push(new Divisa("Peso Argentino", "ARS", "$", 126.74));
 }
 
-function CargarConversionesRecientes(){
+function CargarConversionesRecientes() {
     conversionesRecientes = JSON.parse(localStorage.getItem("ConversionesRecientes")) || []; //Operador OR
+    horariosRecientes = JSON.parse(localStorage.getItem("HorariosRecientes")) || [];
 }
 
-function GenerarConversionesRecientes(){
-    for(let i = 0; i < 5; i++){
+function GenerarConversionesRecientes() {
+    for (let i = 0; i < 5; i++) {
         let nuevaConversion = document.createElement("div");
         nuevaConversion.classList.add("conversion-reciente");
         document.getElementById("conversiones-recientes").append(nuevaConversion);
@@ -33,11 +36,11 @@ function GenerarConversionesRecientes(){
     MostrarConversionesRecientes();
 }
 
-function MostrarConversionesRecientes (){
+function MostrarConversionesRecientes() {
     let conversionesRecientesHTML = document.getElementsByClassName("conversion-reciente");
-    for(let i = 0; i < conversionesRecientes.length; i++){
-        conversionesRecientesHTML[i].innerText = conversionesRecientes[i];
-    }  
+    for (let i = 0; i < conversionesRecientes.length; i++) {
+        conversionesRecientesHTML[i].innerHTML = `<div class="resultado-conversion">${conversionesRecientes[i]}</div><div class="horario-conversion">${horariosRecientes[i]}</div>`;
+    }
 }
 
 function CalcularConversion(divisaPorConvertir, valor, divisaAConvertir) {
@@ -46,7 +49,22 @@ function CalcularConversion(divisaPorConvertir, valor, divisaAConvertir) {
     return resultado;
 }
 
-function ConvertirNumero(){
+function GuardarResultadoActual(resultadoActual){
+    conversionesRecientes.unshift(resultadoActual);
+}
+
+function GuardarHorarioActual(horarioActual){
+    let hora = horarioActual.c.hour;
+    let minuto = horarioActual.c.minute;
+    let dia = horarioActual.c.day;
+    let mes = horarioActual.c.month;
+    let año = horarioActual.c.year;
+
+    horario = `${hora}:${minuto} - ${dia}/${mes}/${año}`;
+    horariosRecientes.unshift(horario);
+}
+
+function ConvertirNumero() {
     let cantidadAConvertir = document.getElementById("cantidadAConvertir").value;
     let divisaPorConvertir = document.getElementById("divisaPorConvertir");
     let divisaAConvertir = document.getElementById("divisaAConvertir");
@@ -55,13 +73,16 @@ function ConvertirNumero(){
     let unidadPorConvertir = divisaPorConvertir.value;
     let unidadAConvertir = divisaAConvertir.value;
 
-    if(!isNaN(cantidadAConvertir) && cantidadAConvertir != ""){
+    if (!isNaN(cantidadAConvertir) && cantidadAConvertir != "") {
         resultadoConversion = CalcularConversion(divisas[divisaPorConvertir.selectedIndex], cantidadAConvertir, divisas[divisaAConvertir.selectedIndex]).toFixed(2);
         lblResultado.innerText = (`${cantidadAConvertir} ${unidadPorConvertir} = ${resultadoConversion} ${unidadAConvertir}`);
-        conversionesRecientes.unshift(lblResultado.innerText);
+        GuardarResultadoActual(lblResultado.innerText);
+        GuardarHorarioActual(DateTime.now());
         conversionesRecientes.length > 5 && conversionesRecientes.pop(); //Operador AND 
+        horariosRecientes.length > 5 && horariosRecientes.pop();
         MostrarConversionesRecientes();
         localStorage.setItem("ConversionesRecientes", JSON.stringify(conversionesRecientes));
+        localStorage.setItem("HorariosRecientes", JSON.stringify(horariosRecientes));
     } else {
         lblResultado.innerText = "Ingrese un número válido"
     }
